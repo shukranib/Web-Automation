c9d84158a657a9ac24dd851ed0af96893fb726ea
shukranib <shukrani.bille@craftsvilla.com>
2016-05-06 14:27:42 +0530

    Changes compare logic

--- src/main/java/com/craftsvilla/commonflow/WishList.java
+++ src/main/java/com/craftsvilla/commonflow/WishList.java
@@ -8,15 +8,17 @@ import com.craftsvilla.pageObjects.HomePage;
 public class WishList {
 
 	public static boolean addToWishListAfterRegister(WebDriver driver) {
-
+		//LoginFlow login=new LoginFlow();
 		Register.registerFlow(driver);
 		HomePage.redirecttoHomePage(driver);
+		//login.loginflow(driver);
 		HomePage.clickOnFistCategory(driver);
 		Wait.defaultHighWait(driver);
 		HomePage.clickOnFirstProduct(driver);
 
 		Wait.defaultHighWait(driver);
 		String productURL = HomePage.getCurrentpageURL(driver);
+		System.out.println("URL IS"+productURL);
 		HomePage.clickOnWishButton(driver);
 		Wait.defaultMediumWait(driver);
 		HomePage.clickOnAccount(driver);
@@ -26,7 +28,10 @@ public class WishList {
 		Wait.defaultMediumWait(driver);
 		HomePage.openFirstProductFromWishList(driver);
 		String wishlistproductURl = HomePage.getCurrentpageURL(driver);
-		if (productURL.equals(wishlistproductURl)) {
+		System.out.println("URL IS"+wishlistproductURl);
+		if(wishlistproductURl.replace('/', ' ').trim().equals(productURL.replace('/', ' ').trim()))
+		{
+			
 			return true;
 		}
 		return false;
