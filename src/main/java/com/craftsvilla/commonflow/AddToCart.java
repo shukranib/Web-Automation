858d50bbc5a8e89cb9858f983aa61d0a35cc850d
shukranib <shukrani.bille@craftsvilla.com>
2016-09-19 18:05:15 +0530

    added new tc

--- src/main/java/com/craftsvilla/commonflow/AddToCart.java
+++ src/main/java/com/craftsvilla/commonflow/AddToCart.java
@@ -22,7 +22,8 @@ public class AddToCart {
 	}
 
 	public static void addToCartWithoutLogin(WebDriver driver) {
-		driver.get(read.getPropertyValue("url") + "/catalog/product/view/id/4331545/s/test");
+		driver.get(read.getPropertyValue("url")
+				+ "/catalog/product/view/id/4331545/s/test?utm_medium=CVTEST&utm_source=CVTEST&utm_campaign=CVTEST");
 
 		/*HomePage.clickOnFistCategory(driver);
 		Wait.defaultHighWait(driver);
